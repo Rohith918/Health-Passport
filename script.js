77a25409fc7d03e5a7b2babe74ff4fb8ed54b814
Rohith_Nallamada <64596471+Rohith918@users.noreply.github.com>
2025-06-05 12:54:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function login() {
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+    const message = document.getElementById("message");
+  
+    if (isStrongPassword(pass)) {
+      message.style.color = "lightgreen";
+      message.textContent = "Login successful! ";
+      
+      setTimeout(() => {
+        window.location.href = "index2.html";
+      }, 1000); 
+    }
+    else{
+        message.style.color = "salmon";
+        message.textContent = "Invalid username or password";
+    }
+  }
+function isStrongPassword(pwd) {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/.test(pwd);
+  }
